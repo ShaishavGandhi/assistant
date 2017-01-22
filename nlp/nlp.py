482b948a90562b577b4b23a4c1b37c4f3a2d809e
shaishavgandhi05 <shaishgandhi@gmail.com>
2017-01-21 16:51:58 -0800

    Transition to api.ai
    Signed-off-by: Shaishav Gandhi <shaishav.gandhi@glassdoor.com>

--- nlp/nlp.py
+++ nlp/nlp.py
@@ -4,10 +4,13 @@ from weather.weather import Weather
 from utils.format import FormatUtils
 from maps.map import Map
 from search.search import Wolfram
+import apiai
+import json
 
 class WitHelper(object):
 
 	access_token = "L44BYDXHQAP2DSB6E2HKTNLZGTFMHTBL"
+	ai_access_token = "ecdea114b2d341a68c3e5a11c5ab4a31"
 	intent_weather = "weather"
 	intent_directions = "directions"
 	intent_search = "search"
@@ -26,32 +29,36 @@ class WitHelper(object):
 
 	def __init__(self):
 		self.responseCreator = ResponseCreator();
-		self.client = Wit(access_token = self.access_token, actions = self.actions)
+		self.ai = apiai.ApiAI(self.ai_access_token)
 
 	def query(self, query):
-		resp = self.client.message(query)
-		self.classify(resp["entities"])
+		request = self.ai.text_request()
+		request.query = query
+		resp = request.getresponse().read()
+		print resp
+		resp_dict = json.loads(resp)
+		self.classify(resp_dict)
 
 	def classify(self, entities):
-		print entities
-		if "intent" in entities.keys():
-			intent = entities["intent"][0]["value"]
-			confidence = entities["intent"][0]["confidence"]
-		else:
-			query = entities["wolfram_search_query"][0]["value"]
-			self.executeSearch(query)
-			return
+		entities = entities["result"]
+		if "metadata" in entities.keys():
+			intent = entities["metadata"]["intentName"]
+			confidence = entities["score"]
+		# else:
+		# 	query = entities["wolfram_search_query"][0]["value"]
+		# 	self.executeSearch(query)
+		# 	return
 
 		if intent == self.intent_weather:
-			location = entities["location"][0]["value"]
+			location = entities["parameters"]["geo-city"]
 			formattedDate = None
-			if "datetime" in entities.keys():
-				date = entities["datetime"][0]["values"][0]["value"] # type : 2017-01-07T00:00:00.000-08:00
+			if entities["parameters"]["date"] != "":
+				date = entities["parameters"]["date"]# type : 2017-01-07T00:00:00.000-08:00
 				formattedDate = FormatUtils().getFormattedDate(date)
 			self.executeWeather(location, formattedDate)
 
 		if intent == self.intent_directions:
-			destination = entities["location"][0]["value"]
+			destination = entities["parameters"]["address"]
 			self.executeDirections(None, destination)
 
 		if intent == self.intent_search:
